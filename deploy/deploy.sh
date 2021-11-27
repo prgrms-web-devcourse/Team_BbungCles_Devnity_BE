53695e99ef0c680c04f915d75d69e35f204c0e11
한재원 <hanjo8813@gmail.com>
2021-11-27 19:08:41 +0900

    chore : 배포 스크립트 작성 (#2)

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,18 +5,18 @@ EXIST_BLUE=$(docker ps | grep blue)
 ## 만약 blue(8081)가 구동중이 아니라면? : blue(8081)를 실행 -> green(8082)은 종료
 if [ -z "$EXIST_BLUE" ]; then
     echo "@@@@@@@@@@@@@@@@@@@@@@ RUN BLUE @@@@@@@@@@@@@@@@@@@@@@"
-    docker-compose -p board-blue -f docker-compose.blue.yml up -d
+    docker-compose -p devnity-blue -f docker-compose.blue.yml up -d
 
     sleep 60  # 서버가 켜지는동안 기다려준다
 
     echo "@@@@@@@@@@@@@@@@@@@@@@ DOWN GREEN @@@@@@@@@@@@@@@@@@@@@@"
-    docker-compose -p board-green -f docker-compose.green.yml down
+    docker-compose -p devnity-green -f docker-compose.green.yml down
 else
     echo "@@@@@@@@@@@@@@@@@@@@@@ RUN GREEN @@@@@@@@@@@@@@@@@@@@@@"
-    docker-compose -p board-green -f docker-compose.green.yml up -d
+    docker-compose -p devnity-green -f docker-compose.green.yml up -d
 
     sleep 60
 
     echo "@@@@@@@@@@@@@@@@@@@@@@ DOWN BLUE @@@@@@@@@@@@@@@@@@@@@@"
-    docker-compose -p board-blue -f docker-compose.blue.yml down
+    docker-compose -p devnity-blue -f docker-compose.blue.yml down
 fi
