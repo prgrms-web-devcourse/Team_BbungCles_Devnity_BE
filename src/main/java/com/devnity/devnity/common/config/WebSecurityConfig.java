9f09599e67a88210fcebd718774b38b4ea59eeaa
한재원 <hanjo8813@gmail.com>
2021-12-02 23:25:07 +0900

    chore : application.yml 환경 분리 (#10)

--- src/main/java/com/devnity/devnity/common/config/WebSecurityConfig.java
+++ src/main/java/com/devnity/devnity/common/config/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-//package com.devnity.devnity.config;
+//package com.devnity.devnity.common.config;
 //
 //import org.springframework.context.annotation.Configuration;
 //import org.springframework.security.config.annotation.web.builders.HttpSecurity;
---
+++ src/main/java/com/devnity/devnity/common/utils/AwsS3Uploader.java
@@ -0,0 +1,4 @@
+package com.devnity.devnity.common.utils;
+
+public class AwsS3Uploader {
+}
