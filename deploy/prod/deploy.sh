5e4f8fb5bd8d450a20e4a2f2f3edf7129d79828d
Jaewon Han <hanjo8813@gmail.com>
2021-12-03 14:41:50 +0900

    chore: develop 브랜치에 ci/cd 적용 + slack 연동 (#12)
    * chore: dev 배포 스크립트 작성 (#2)
    
    * chore: dev 배포 스크립트 작성 (#2)
    
    * chore : github actions와 slack 연동 (#7)
    
    * chore : 다시 push
    
    * chore : yml 문법 수정
    
    * chore: git cache 삭제
    
    * chore: git cache 삭제

--- deploy/prod/deploy.sh
+++ deploy/prod/deploy.sh
@@ -11,10 +11,6 @@ AWS_S3_BUCKET=$7
 AWS_S3_ACCESS_KEY=$8
 AWS_S3_SECRET_KEY=$9
 
-# 빌드된 최신 이미지 pull
-echo "================== PULL docker image =================="
-docker pull $DOCKER_USERNAME/$DOCKER_REPOSITORY:$DOCKER_TAG_PROD
-
 # 환경변수로 docker-compose 전체용 .env 파일 생성
 echo "================== UPDATE '.env' file =================="
 echo -e "DOCKER_USERNAME=$DOCKER_USERNAME\nDOCKER_REPOSITORY=$DOCKER_REPOSITORY\nDOCKER_TAG_PROD=$DOCKER_TAG_PROD" > .env
@@ -22,6 +18,10 @@ echo -e "DOCKER_USERNAME=$DOCKER_USERNAME\nDOCKER_REPOSITORY=$DOCKER_REPOSITORY\
 # docker-compose spring 컨테이너용 spring.env 파일 생성
 echo "================== UPDATE 'spring.env' file =================="
 echo -e "DB_URL_PROD=$DB_URL_PROD\nDB_USERNAME=$DB_USERNAME\nDB_PASSWORD=$DB_PASSWORD\nAWS_S3_BUCKET=$AWS_S3_BUCKET\nAWS_S3_ACCESS_KEY=$AWS_S3_ACCESS_KEY\nAWS_S3_SECRET_KEY=$AWS_S3_SECRET_KEY" > spring.env
+
+# 빌드된 최신 이미지 pull
+echo "================== PULL docker image =================="
+docker pull $DOCKER_USERNAME/$DOCKER_REPOSITORY:$DOCKER_TAG_PROD
 
 ## 만약 blue(8081)가 구동중이 아니라면? : blue(8081)를 실행 -> green(8082)은 종료
 EXIST_BLUE=$(docker ps | grep blue)
