12a5d150f5034d684f7ed69307280a8a80c9d7a6
seunghun <dau4723@gmail.com>
2021-12-03 12:51:50 +0900

    fix: 오타로 인한 에러 해결

--- src/main/java/com/devnity/devnity/gather/repository/GatherRepository.java
+++ src/main/java/com/devnity/devnity/gather/repository/GatherRepository.java
@@ -3,6 +3,6 @@ package com.devnity.devnity.gather.repository;
 import com.devnity.devnity.gather.domain.Gather;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class GatherRepository extends JpaRepository<Gather, Long> {
+public interface GatherRepository extends JpaRepository<Gather, Long> {
 
 }
--- src/main/java/com/devnity/devnity/user/repository/UserRepository.java
+++ src/main/java/com/devnity/devnity/user/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.devnity.devnity.user.repository;
 
+import com.devnity.devnity.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
