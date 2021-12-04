be52fb8bf24df3c9879419128ea00ad85976a292
seunghun <dau4723@gmail.com>
2021-12-04 13:20:37 +0900

    chore: gitignore 추가

--- src/main/java/com/devnity/devnity/domain/user/entity/Group.java
+++ src/main/java/com/devnity/devnity/domain/user/entity/Group.java
@@ -1,14 +1,22 @@
 package com.devnity.devnity.domain.user.entity;
 
+import static java.util.stream.Collectors.toList;
+
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
@@ -22,9 +30,16 @@ public class Group {
   @Column(name = "name", unique = true, nullable = false, length = 10)
   private String name;
 
+  @OneToMany(mappedBy = "group")
+  private List<GroupPermission> permissions = new ArrayList<>();
+
   public Group(String name) {
     this.name = name;
   }
 
-
+  public List<GrantedAuthority> getAuthorities() {
+    return permissions.stream()
+        .map(gp -> new SimpleGrantedAuthority(gp.getPermission().getName()))
+        .collect(toList());
+  }
 }
