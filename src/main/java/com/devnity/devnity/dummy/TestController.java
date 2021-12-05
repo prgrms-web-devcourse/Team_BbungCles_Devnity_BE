bb7a3c7b9d47121c9e4651f5e98ca84624a5a5bd
한재원 <hanjo8813@gmail.com>
2021-12-06 06:20:39 +0900

    feat: @RequestPart를 사용하여 요청의 file과 json을 분리
    이미지 업로더도 그에 맞춰 수정

--- src/main/java/com/devnity/devnity/dummy/TestController.java
+++ src/main/java/com/devnity/devnity/dummy/TestController.java
@@ -2,10 +2,8 @@ package com.devnity.devnity.dummy;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,15 +13,23 @@ import java.util.Map;
 @RestController
 public class TestController {
 
-  private final TestService testService;
+    private final TestService testService;
 
-  @PostMapping
-  public Map<String, String> insertDummy() {
-    return testService.insertDummy();
-  }
+    @PostMapping
+    public Map<String, String> insertDummy() {
+        return testService.insertDummy();
+    }
 
-  @PostMapping("/image")
-  public Map<String, String> insertImage(@RequestBody ImageRequest request) {
-    return testService.insertImage(request);
-  }
+    @PostMapping("/image")
+    public Map<String, String> insertImage(@RequestBody ImageRequest request) {
+        return testService.insertImage(request);
+    }
+
+    @PostMapping("/image/temp")
+    public Map<String, String> insertImageTemp(
+            @RequestPart(value = "file", required = false) MultipartFile imageFile,
+            @RequestPart(value = "json") JsonRequest request
+    ) {
+        return testService.insertImageTemp(imageFile, request);
+    }
 }
--- src/main/java/com/devnity/devnity/dummy/TestService.java
+++ src/main/java/com/devnity/devnity/dummy/TestService.java
@@ -1,31 +1,47 @@
 package com.devnity.devnity.dummy;
 
 import com.devnity.devnity.common.utils.AwsS3Uploader;
+import com.devnity.devnity.common.utils.AwsS3UploaderTemp;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.awt.*;
+import java.io.IOException;
 import java.util.Map;
 
+@Transactional
 @RequiredArgsConstructor
 @Service
 public class TestService {
 
   private final AwsS3Uploader awsS3Uploader;
+  private final AwsS3UploaderTemp awsS3UploaderTemp;
+
   private final TestRepository testRepository;
 
-  @Transactional
+
   public Map<String, String> insertDummy() {
     TestEntity dummy = TestEntity.builder().str("dummy").build();
     testRepository.save(dummy);
     return Map.of("result", "success - insert dummy");
   }
 
-  @Transactional
   public Map<String, String> insertImage(ImageRequest request) {
     String resultUrl = awsS3Uploader.upload(request.getImageBase64(), "test");
+    return Map.of("resultUrl", resultUrl);
+  }
+
+  public Map<String, String> insertImageTemp(MultipartFile imageFile, JsonRequest request){
+    System.out.println(request.getNum());
+    System.out.println(request.getText());
+
+    String resultUrl = awsS3UploaderTemp.upload(imageFile, "test");
+    if(resultUrl == null){
+      resultUrl = "널이에용";
+    }
     return Map.of("resultUrl", resultUrl);
   }
 }
