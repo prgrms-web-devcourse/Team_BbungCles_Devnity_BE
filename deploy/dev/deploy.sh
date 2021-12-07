293c89a0e741abf1d20645110d9408e53beabb65
한재원 <hanjo8813@gmail.com>
2021-12-07 13:37:18 +0900

    chore: JWT 인자 환경변수로 변경 (#17)

--- deploy/dev/deploy.sh
+++ deploy/dev/deploy.sh
@@ -7,9 +7,12 @@ DOCKER_TAG_DEV=$3
 DB_URL_DEV=$4
 DB_USERNAME=$5
 DB_PASSWORD=$6
-AWS_S3_BUCKET=$7
-AWS_S3_ACCESS_KEY=$8
-AWS_S3_SECRET_KEY=$9
+AWS_S3_ACCESS_KEY=$7
+AWS_S3_SECRET_KEY=$8
+AWS_CLOUDFRONT_URL=$9
+JWT_EXPIRE=$10
+JWT_ISSUER=$11
+JWT_SECRET=$12
 
 # 환경변수로 docker-compose 전체용 .env 파일 생성
 echo "================== UPDATE '.env' file =================="
@@ -17,7 +20,7 @@ echo -e "DOCKER_USERNAME=$DOCKER_USERNAME\nDOCKER_REPOSITORY=$DOCKER_REPOSITORY\
 
 # docker-compose spring 컨테이너용 spring.env 파일 생성
 echo "================== UPDATE 'spring.env' file =================="
-echo -e "DB_URL_DEV=$DB_URL_DEV\nDB_USERNAME=$DB_USERNAME\nDB_PASSWORD=$DB_PASSWORD\nAWS_S3_BUCKET=$AWS_S3_BUCKET\nAWS_S3_ACCESS_KEY=$AWS_S3_ACCESS_KEY\nAWS_S3_SECRET_KEY=$AWS_S3_SECRET_KEY" > spring.env
+echo -e "DB_URL_DEV=$DB_URL_DEV\nDB_USERNAME=$DB_USERNAME\nDB_PASSWORD=$DB_PASSWORD\nAWS_S3_ACCESS_KEY=$AWS_S3_ACCESS_KEY\nAWS_S3_SECRET_KEY=$AWS_S3_SECRET_KEY\nAWS_CLOUDFRONT_URL=$AWS_CLOUDFRONT_URL\nJWT_EXPIRE=$JWT_EXPIRE\nJWT_ISSUER=$JWT_ISSUER\nJWT_SECRET=$JWT_SECRET" > spring.env
 
 # docker-compose 재실행
 echo "================== PULL docker image =================="
@@ -28,3 +31,5 @@ echo "================== SERVER UP   =================="
 docker-compose -p compose_dev up -d
 echo "================== DELETE unused images  =================="
 docker image prune -f
+
+
