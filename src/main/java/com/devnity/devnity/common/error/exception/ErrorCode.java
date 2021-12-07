bf55e5f493da574fd3781ea731c54136e659f9a2
전찬의 <jcu011@naver.com>
2021-12-07 16:11:13 +0900

    style: 코딩 컨벤션 tap간격 2로 수정

--- src/main/java/com/devnity/devnity/common/error/exception/ErrorCode.java
+++ src/main/java/com/devnity/devnity/common/error/exception/ErrorCode.java
@@ -6,41 +6,41 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum ErrorCode {
 
-    // Common
-    INVALID_INPUT_VALUE(400, "Invalid Input Value"),
-    METHOD_NOT_ALLOWED(405, "Method not allowed"),
-    ENTITY_NOT_FOUND(400, "Entity Not Found"),
-    INTERNAL_SERVER_ERROR(500, "Server Error"),
-    INVALID_TYPE_VALUE(400, "Invalid Type Value"),
-    HANDLE_ACCESS_DENIED(403, "Access is Denied"),
+  // Common
+  INVALID_INPUT_VALUE(400, "Invalid Input Value"),
+  METHOD_NOT_ALLOWED(405, "Method not allowed"),
+  ENTITY_NOT_FOUND(400, "Entity Not Found"),
+  INTERNAL_SERVER_ERROR(500, "Server Error"),
+  INVALID_TYPE_VALUE(400, "Invalid Type Value"),
+  HANDLE_ACCESS_DENIED(403, "Access is Denied"),
 
-    // User
+  // User
 
-    // Introduction
+  // Introduction
 
-    // Mapgakco
+  // Mapgakco
 
-    // Gather
+  // Gather
 
-    // jwt
+  // jwt
 
-    ;
+  ;
 
-    private final String message;
-    private int status;
+  private final String message;
+  private int status;
 
-    ErrorCode(final int status, final String message) {
-        this.status = status;
-        this.message = message;
-    }
+  ErrorCode(final int status, final String message) {
+    this.status = status;
+    this.message = message;
+  }
 
-    public String getMessage() {
-        return this.message;
-    }
+  public String getMessage() {
+    return this.message;
+  }
 
-    public int getStatus() {
-        return status;
-    }
+  public int getStatus() {
+    return status;
+  }
 
 
 }
