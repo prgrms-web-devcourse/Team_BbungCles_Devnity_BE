0227f6b7fd9c0f6dda98b4ab8c6ca9bf0f0c1364
전찬의 <jcu011@naver.com>
2021-12-07 19:24:36 +0900

    test: test코드 수정

--- src/test/java/com/devnity/devnity/common/entity/BaseEntityTest.java
+++ src/test/java/com/devnity/devnity/common/entity/BaseEntityTest.java
@@ -2,7 +2,7 @@ package com.devnity.devnity.common.entity;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import javax.persistence.Entity;
 import javax.persistence.EntityManager;
 import javax.persistence.GeneratedValue;
@@ -12,8 +12,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional(readOnly = true)
 class BaseEntityTest {
 
   @Autowired
@@ -32,24 +34,23 @@ class BaseEntityTest {
   }
 
   private Long id;
-  private LocalDateTime createdAt;
-  private LocalDateTime modifiedAt;
 
   @BeforeEach
+  @Transactional
   void setUp() throws Exception {
     TestClass entity = new TestClass();
     em.persist(entity);
-    Long id = entity.getId();
-    createdAt = entity.getCreatedAt();
-    modifiedAt = entity.getModifiedAt();
+    id = entity.getId();
   }
 
   @Test
   @DisplayName("BaseEntity의 createdAt과 modifiedAt이 잘 생성됨")
   void findEntity() {
     TestClass testClass = em.find(TestClass.class, id);
-    assertThat(testClass.getCreatedAt()).isEqualTo(createdAt);
-    assertThat(testClass.getModifiedAt()).isEqualTo(modifiedAt);
+    assertThat(testClass).isNotNull();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy LLLL dd일 hh:mm:ss");
+    System.out.println("createdAt : " + testClass.getCreatedAt().format(formatter));
+    System.out.println("modifiedAt : " + testClass.getModifiedAt().format(formatter));
   }
 
 }
