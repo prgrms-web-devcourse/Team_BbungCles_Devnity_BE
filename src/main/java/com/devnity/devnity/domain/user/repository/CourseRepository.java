315a6d458e79110251c5693ea3b784ef8659f478
seunghun <dau4723@gmail.com>
2021-12-07 20:03:39 +0900

    feat: 회원가입 비즈니스 로직 작성(#22)

--- src/main/java/com/devnity/devnity/domain/user/repository/CourseRepository.java
+++ src/main/java/com/devnity/devnity/domain/user/repository/CourseRepository.java
@@ -4,4 +4,6 @@ import com.devnity.devnity.domain.user.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
+
+  Course findByName(String name);
 }
--- src/main/java/com/devnity/devnity/domain/user/service/UserService.java
+++ src/main/java/com/devnity/devnity/domain/user/service/UserService.java
@@ -1,10 +1,18 @@
 package com.devnity.devnity.domain.user.service;
 
+import com.devnity.devnity.domain.user.dto.request.SignUpRequest;
 import com.devnity.devnity.domain.user.dto.UserDto;
 import com.devnity.devnity.domain.user.dto.response.UserInfoResponse;
+import com.devnity.devnity.domain.user.entity.Course;
+import com.devnity.devnity.domain.user.entity.Generation;
+import com.devnity.devnity.domain.user.entity.Group;
 import com.devnity.devnity.domain.user.entity.User;
+import com.devnity.devnity.domain.user.repository.CourseRepository;
+import com.devnity.devnity.domain.user.repository.GenerationRepository;
+import com.devnity.devnity.domain.user.repository.GroupRepository;
 import com.devnity.devnity.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,6 +22,14 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
   private final UserRepository userRepository;
+
+  private final CourseRepository courseRepository;
+
+  private final GenerationRepository generationRepository;
+
+  private final GroupRepository groupRepository;
+
+  private final PasswordEncoder passwordEncoder;
 
   public UserInfoResponse getUserInfoBy(Long userId) {
     UserDto userDto = retrieveUser(userId);
@@ -25,12 +41,43 @@ public class UserService {
         .orElseThrow(() -> new IllegalArgumentException(
             String.format("There is no user for id = %d", userId)));
 
-    return UserDto.builder()
+    // TODO: Prifile Image 추가
+   return UserDto.builder()
         .userId(user.getId())
         .course(user.getCourse())
         .generation(user.getGeneration())
         .name(user.getName())
         .role(user.getRole().toString())
         .build();
+  }
+
+  @Transactional
+  public Long signUp(SignUpRequest request) {
+
+    checkDuplicatedEmail(request.getEmail());
+
+    Course course = UserServiceUtils.findCourse(courseRepository, request.getCourse());
+
+    Generation generation = UserServiceUtils.findGeneration(generationRepository,
+        request.getGeneration());
+
+    Group group = UserServiceUtils.findGroup(
+        groupRepository, request.getRole());
+
+    User user = request.toEntity(passwordEncoder, group, course, generation);
+
+    userRepository.save(user);
+
+    return user.getId();
+  }
+
+  private void checkDuplicatedEmail(String email) {
+    if (existsByEmail(email)) {
+      throw new IllegalArgumentException(String.format("Email is duplicated. email=%s", email));
+    }
+  }
+
+  public boolean existsByEmail(String email) {
+    return userRepository.existsByEmail(email);
   }
 }
