5cc6d6c8003d2a0f40b7f002e8c608016b78fcb0
seunghun <dau4723@gmail.com>
2021-12-07 22:46:31 +0900

    feat: 이메일 중복 확인 기능 구현

--- src/main/java/com/devnity/devnity/domain/user/controller/UserController.java
+++ src/main/java/com/devnity/devnity/domain/user/controller/UserController.java
@@ -4,12 +4,15 @@ import com.devnity.devnity.domain.auth.jwt.JwtAuthentication;
 import com.devnity.devnity.domain.user.dto.request.SignUpRequest;
 import com.devnity.devnity.domain.user.dto.response.UserInfoResponse;
 import com.devnity.devnity.domain.user.service.UserService;
+import java.util.Collections;
+import java.util.Map;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,5 +36,10 @@ public class UserController {
   public ResponseEntity<Void> signUp(@Valid @RequestBody SignUpRequest request) {
     userService.signUp(request);
     return ResponseEntity.ok().build();
+  }
+
+  @GetMapping("/{email}/check")
+  public ResponseEntity<Map> checkEmail(@PathVariable String email) {
+    return ResponseEntity.ok(Collections.singletonMap("isDuplicated", userService.existsByEmail(email)));
   }
 }
