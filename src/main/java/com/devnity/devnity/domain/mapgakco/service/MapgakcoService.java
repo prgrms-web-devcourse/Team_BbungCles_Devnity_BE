48bb7bc9998c303af0c36a0b75c8b7bdb8bc6bf3
전찬의 <jcu011@naver.com>
2021-12-09 04:21:16 +0900

    feat: MapgakcoService에 맵각코 등록 로직 추가

--- src/main/java/com/devnity/devnity/domain/mapgakco/service/MapgakcoService.java
+++ src/main/java/com/devnity/devnity/domain/mapgakco/service/MapgakcoService.java
@@ -1,6 +1,12 @@
 package com.devnity.devnity.domain.mapgakco.service;
 
+import com.devnity.devnity.common.error.exception.EntityNotFoundException;
+import com.devnity.devnity.domain.mapgakco.converter.MapgakcoConverter;
+import com.devnity.devnity.domain.mapgakco.dto.mapgakco.MapgakcoCreateRequest;
+import com.devnity.devnity.domain.mapgakco.entity.MapgakcoStatus;
 import com.devnity.devnity.domain.mapgakco.repository.MapgakcoRepository;
+import com.devnity.devnity.domain.user.entity.User;
+import com.devnity.devnity.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,6 +16,16 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class MapgakcoService {
 
+  private final MapgakcoConverter mapgakcoConverter;
   private final MapgakcoRepository mapgakcoRepository;
+  private final UserRepository userRepository;
+
+  @Transactional
+  public MapgakcoStatus create(Long userId, MapgakcoCreateRequest request) {
+    // Todo : 중복코드 리펙토링 -> facade 클래스
+    User user = userRepository.findById(userId)
+      .orElseThrow(() -> new EntityNotFoundException("User is not found."));
+    return mapgakcoRepository.save(mapgakcoConverter.toMapgakco(user, request)).getStatus();
+  }
 
 }
