7e5816f46d4268e96265c45897f772b6a7d483f8
전찬의 <jcu011@naver.com>
2021-12-09 03:39:14 +0900

    feat: MapgakcoCreateRequest, Converter 작성

---
+++ src/main/java/com/devnity/devnity/domain/mapgakco/converter/MapgakcoConverter.java
@@ -0,0 +1,30 @@
+package com.devnity.devnity.domain.mapgakco.converter;
+
+import com.devnity.devnity.domain.mapgakco.dto.mapgakco.MapgakcoCreateRequest;
+import com.devnity.devnity.domain.mapgakco.entity.Mapgakco;
+import com.devnity.devnity.domain.mapgakco.entity.MapgakcoStatus;
+import com.devnity.devnity.domain.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class MapgakcoConverter {
+
+  public Mapgakco toMapgakco(User user, MapgakcoCreateRequest request) {
+    return Mapgakco.builder()
+      .title(request.getTitle())
+      .applicantLimit(request.getApplicantLimit())
+      .deadline(request.getDeadline())
+      .content(request.getContent())
+      .location(request.getLocation())
+      .latitude(request.getLatitude())
+      .longitude(request.getLongitude())
+      .meetingAt(request.getMeetingDateTime())
+      .user(user)
+      .status(MapgakcoStatus.GATHERING)
+      .view(0)
+      .build();
+  }
+
+}
