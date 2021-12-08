0931f7ca0e092bc6d7c636653b93ea05efad0b08
전찬의 <jcu011@naver.com>
2021-12-09 03:39:14 +0900

    feat: MapgakcoCreateRequest, Converter 작성

--- src/main/java/com/devnity/devnity/domain/mapgakco/dto/mapgakco/MapgakcoCreateRequest.java
+++ src/main/java/com/devnity/devnity/domain/mapgakco/dto/mapgakco/MapgakcoCreateRequest.java
@@ -1,5 +1,25 @@
 package com.devnity.devnity.domain.mapgakco.dto.mapgakco;
 
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
+@Builder
 public class MapgakcoCreateRequest {
-  // temp
+
+  private String title;
+  private Integer applicantLimit;
+  private LocalDateTime deadline;
+  private String content;
+  private String location;
+  private Double latitude;
+  private Double longitude;
+  private LocalDateTime meetingDateTime;
+
 }
