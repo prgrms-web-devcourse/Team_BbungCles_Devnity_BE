e718ffe2f72cff09a0514960529cd3b0fae002d0
전찬의 <jcu011@naver.com>
2021-12-09 01:29:12 +0900

    feat: Mapgakco domain service init

---
+++ src/main/java/com/devnity/devnity/domain/mapgakco/service/MapgakcoApplicantService.java
@@ -0,0 +1,15 @@
+package com.devnity.devnity.domain.mapgakco.service;
+
+import com.devnity.devnity.domain.mapgakco.repository.MapgakcoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MapgakcoApplicantService {
+
+    private final MapgakcoRepository mapgakcoRepository;
+
+}
