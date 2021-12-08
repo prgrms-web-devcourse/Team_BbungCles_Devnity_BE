c6cbe6cf34a8ef27102ebb6844a0ed7f5b7c64b2
전찬의 <jcu011@naver.com>
2021-12-09 04:42:49 +0900

    refactor: CursorPageResponse 필드 추가

--- src/main/java/com/devnity/devnity/common/api/CursorPageResponse.java
+++ src/main/java/com/devnity/devnity/common/api/CursorPageResponse.java
@@ -9,7 +9,9 @@ import lombok.Getter;
 public class CursorPageResponse<T> {
 
   private List<T> values;
-  private Long nextLastId;
-  private Boolean hasNext;
-
+  private Long nextLastId; // 다음 페이지의 lastId, 다음 페이지가 없으면 null
+  private Boolean isFirstPage; // 현재 페이지가 첫 페이지 인지 여부
+  private Boolean isLastPage; // 현재 페이지가 마지막 페이지 인지 여부
+  private Integer numberOfElement; // 현재 페이지에 나올 데이터 수
+  private Integer totalPages; // 전체 페이지 수
 }
