90daf39b3d4dd11127c6e0f16236f8f88d7694d9
seunghun <dau4723@gmail.com>
2021-12-08 18:41:09 +0900

    chore: POSTED 상태 자기소개 조회하도록 변경

--- src/main/java/com/devnity/devnity/domain/introduction/respository/IntroductionRepository.java
+++ src/main/java/com/devnity/devnity/domain/introduction/respository/IntroductionRepository.java
@@ -3,8 +3,10 @@ package com.devnity.devnity.domain.introduction.respository;
 import com.devnity.devnity.domain.introduction.entity.Introduction;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface IntroductionRepository extends JpaRepository<Introduction, Long> {
 
+  @Query("select i from Introduction i where i.id = :id and i.user.id = :userId and i.status = 'POSTED'")
   Optional<Introduction> findByIdAndUserId(Long id, Long userId);
 }
