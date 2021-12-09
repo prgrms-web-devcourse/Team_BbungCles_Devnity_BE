681b21632ce3836e387390e655dcbeb638c6edea
seunghun <dau4723@gmail.com>
2021-12-09 09:38:25 +0900

    test: spring security test annotation

---
+++ src/test/java/com/devnity/devnity/test/annotation/WithJwtAuthUser.java
@@ -0,0 +1,14 @@
+package com.devnity.devnity.test.annotation;
+
+import com.devnity.devnity.test.config.WithJwtAuthUserSecurityContext;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import org.springframework.security.test.context.support.WithSecurityContext;
+
+@Retention(RetentionPolicy.RUNTIME)
+@WithSecurityContext(factory = WithJwtAuthUserSecurityContext.class)
+public @interface WithJwtAuthUser {
+
+  String email();
+  String[] roles();
+}
