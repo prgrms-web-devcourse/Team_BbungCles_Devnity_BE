458d52f28ad06dc06e1d5f417875164504152406
전찬의 <jcu011@naver.com>
2021-12-09 16:50:04 +0900

    refactor: Mapgakco Converter 피드백 수정

--- src/main/java/com/devnity/devnity/domain/mapgakco/converter/MapgakcoConverter.java
+++ src/main/java/com/devnity/devnity/domain/mapgakco/converter/MapgakcoConverter.java
@@ -3,11 +3,9 @@ package com.devnity.devnity.domain.mapgakco.converter;
 import com.devnity.devnity.domain.mapgakco.dto.mapgakco.MapgakcoCreateRequest;
 import com.devnity.devnity.domain.mapgakco.entity.Mapgakco;
 import com.devnity.devnity.domain.user.entity.User;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class MapgakcoConverter {
 
   public Mapgakco toMapgakco(User user, MapgakcoCreateRequest request) {
