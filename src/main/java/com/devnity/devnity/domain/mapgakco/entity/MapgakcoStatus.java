1cd5797a7c979606e970311983a36c3d07a34a25
전찬의 <jcu011@naver.com>
2021-12-10 02:07:53 +0900

    refactor: enum파일 수정, 직렬화코드 추가

--- src/main/java/com/devnity/devnity/domain/mapgakco/entity/MapgakcoStatus.java
+++ src/main/java/com/devnity/devnity/domain/mapgakco/entity/MapgakcoStatus.java
@@ -1,8 +1,22 @@
 package com.devnity.devnity.domain.mapgakco.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
 
+@Getter
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum MapgakcoStatus {
-  GATHERING, CLOSED, FULL, DELETED;
+  GATHERING("모집중"),
+  CLOSED("모집마감"),
+  FULL("인원초과"),
+  DELETED("삭제됨");
+
+  @JsonValue
+  private final String status;
+
+  MapgakcoStatus(final String status) {
+    this.status = status;
+  }
+
 }
