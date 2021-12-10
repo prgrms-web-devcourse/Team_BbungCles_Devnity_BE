fae09737380e60cf9b3ace333cfb3b9e6e52ebbd
seunghun-h <dau4723@gmail.com>
2021-12-10 17:26:11 +0900

    chore: UserServiceUtiils 변경

--- src/main/java/com/devnity/devnity/domain/user/service/UserServiceUtils.java
+++ src/main/java/com/devnity/devnity/domain/user/service/UserServiceUtils.java
@@ -2,12 +2,13 @@ package com.devnity.devnity.domain.user.service;
 
 import com.devnity.devnity.domain.user.entity.Course;
 import com.devnity.devnity.domain.user.entity.Generation;
+import com.devnity.devnity.domain.user.entity.User;
 import com.devnity.devnity.domain.user.entity.UserRole;
 import com.devnity.devnity.domain.user.repository.CourseRepository;
 import com.devnity.devnity.domain.user.repository.GenerationRepository;
+import com.devnity.devnity.domain.user.repository.UserRepository;
 import org.springframework.stereotype.Component;
 
-@Component
 public class UserServiceUtils {
   public static Generation findGeneration(GenerationRepository generationRepository, int sequence) {
     return generationRepository.findBySequence(sequence)
@@ -17,5 +18,11 @@ public class UserServiceUtils {
   public static Course findCourse(CourseRepository courseRepository, String name) {
     return courseRepository.findByName(name)
       .orElseThrow(() -> new IllegalArgumentException(String.format("There is no course for name = %s", name)));
+  }
+
+  public static User findUser(UserRepository userRepository, Long userId) {
+    return userRepository.findById(userId)
+      .orElseThrow(() -> new IllegalArgumentException(
+        String.format("There is no user for id = %d", userId)));
   }
 }
