cba1786915770f34979508a62a83847360eabe06
전찬의 <jcu011@naver.com>
2021-12-12 02:11:23 +0900

    refactor: 맵각코 삭제 컨트롤러 추가

--- src/main/java/com/devnity/devnity/domain/mapgakco/controller/MapgakcoV1Controller.java
+++ src/main/java/com/devnity/devnity/domain/mapgakco/controller/MapgakcoV1Controller.java
@@ -7,6 +7,8 @@ import com.devnity.devnity.domain.mapgakco.entity.MapgakcoStatus;
 import com.devnity.devnity.domain.mapgakco.service.MapgakcoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +27,12 @@ public class MapgakcoV1Controller {
       @RequestBody MapgakcoCreateRequest request
     ) {
         return ApiResponse.ok(mapgakcoService.create(jwtAuthentication.getUserId(), request));
+    }
+
+    @DeleteMapping("/mapgakcos/{id}")
+    public ApiResponse<String> deleteMapgakco(@PathVariable Long mapgakcoId) {
+        mapgakcoService.delete(mapgakcoId);
+        return ApiResponse.ok();
     }
 
 }
