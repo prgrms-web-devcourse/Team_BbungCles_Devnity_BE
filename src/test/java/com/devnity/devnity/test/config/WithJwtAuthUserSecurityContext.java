73df7248876c5c3787282a3758d1fa5cc345c90c
seunghun-h <dau4723@gmail.com>
2021-12-11 16:07:11 +0900

    chore: JWT 인증 테스트 어노테이션 변경

--- src/test/java/com/devnity/devnity/test/config/WithJwtAuthUserSecurityContext.java
+++ src/test/java/com/devnity/devnity/test/config/WithJwtAuthUserSecurityContext.java
@@ -4,7 +4,13 @@ import com.devnity.devnity.domain.auth.jwt.Jwt;
 import com.devnity.devnity.domain.auth.jwt.Jwt.Claims;
 import com.devnity.devnity.domain.auth.jwt.JwtAuthentication;
 import com.devnity.devnity.domain.auth.jwt.JwtAuthenticationToken;
+import com.devnity.devnity.domain.user.entity.Authority;
+import com.devnity.devnity.domain.user.entity.Course;
+import com.devnity.devnity.domain.user.entity.Generation;
 import com.devnity.devnity.domain.user.entity.User;
+import com.devnity.devnity.domain.user.entity.UserRole;
+import com.devnity.devnity.domain.user.repository.CourseRepository;
+import com.devnity.devnity.domain.user.repository.GenerationRepository;
 import com.devnity.devnity.domain.user.repository.UserRepository;
 import com.devnity.devnity.test.annotation.WithJwtAuthUser;
 import java.util.Arrays;
@@ -18,31 +24,53 @@ public class WithJwtAuthUserSecurityContext implements WithSecurityContextFactor
 
   private final Jwt jwt;
   private final UserRepository userRepository;
+  private final CourseRepository courseRepository;
+  private final GenerationRepository generationRepository;
 
-  public WithJwtAuthUserSecurityContext(Jwt jwt, UserRepository userRepository) {
+  public WithJwtAuthUserSecurityContext(
+      Jwt jwt,
+      UserRepository userRepository,
+      CourseRepository courseRepository,
+      GenerationRepository generationRepository) {
+
     this.jwt = jwt;
     this.userRepository = userRepository;
+    this.courseRepository = courseRepository;
+    this.generationRepository = generationRepository;
   }
 
   @Override
   public SecurityContext createSecurityContext(WithJwtAuthUser withJwtAuthUser) {
     String email = withJwtAuthUser.email();
-    String role = withJwtAuthUser.role();
-    User user = userRepository
-        .findUserByEmail(email)
-        .orElseThrow(
-            () ->
-                new IllegalArgumentException(
-                    String.format("There is no user for email = %s", email)));
+    UserRole role = withJwtAuthUser.role();
+    String courseName = withJwtAuthUser.course();
+    String name = withJwtAuthUser.name();
+    int sequence = withJwtAuthUser.generation();
+    String password = withJwtAuthUser.password();
 
-    String jwtToken = jwt.sign(Claims.from(user.getId(), email, role));
+    Generation generation = generationRepository.findBySequence(withJwtAuthUser.generation())
+      .orElseGet(() -> generationRepository.save(new Generation(sequence)));
+
+    Course course = courseRepository.findByName(courseName)
+      .orElseGet(() -> courseRepository.save(new Course(courseName)));
+
+    User user = userRepository.save(User.builder()
+      .email(email)
+      .name(name)
+      .role(role)
+      .password(password)
+      .generation(generation)
+      .course(course)
+      .build());
+
+    String jwtToken = jwt.sign(Claims.from(user.getId(), email, role.toString()));
 
     JwtAuthentication authentication = new JwtAuthentication(jwtToken, user.getId(), email);
     JwtAuthenticationToken jwtAuthenticationToken =
         new JwtAuthenticationToken(
             authentication,
             user.getPassword(),
-            new SimpleGrantedAuthority(role));
+            new SimpleGrantedAuthority(role.toString()));
 
     SecurityContextHolder.getContext().setAuthentication(jwtAuthenticationToken);
     return SecurityContextHolder.getContext();
