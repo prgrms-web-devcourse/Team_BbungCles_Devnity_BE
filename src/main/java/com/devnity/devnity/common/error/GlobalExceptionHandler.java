3d132f3ade14c8e80b7c430f8d7904fb8731da6f
전찬의 <jcu011@naver.com>
2021-12-11 22:01:53 +0900

    refactor: BusinessException, Exception 핸들러내 로그 오타 수정

--- src/main/java/com/devnity/devnity/common/error/GlobalExceptionHandler.java
+++ src/main/java/com/devnity/devnity/common/error/GlobalExceptionHandler.java
@@ -76,7 +76,7 @@ public class GlobalExceptionHandler {
 
   @ExceptionHandler(BusinessException.class)
   protected ResponseEntity<ErrorResponse> handleBusinessException(final BusinessException e) {
-    log.error("handleEntityNotFoundException", e);
+    log.error("handleBusinessException", e);
     final ErrorCode errorCode = e.getErrorCode();
     final ErrorResponse response = ErrorResponse.of(errorCode);
     return new ResponseEntity<>(response, HttpStatus.valueOf(errorCode.getStatus()));
@@ -85,7 +85,7 @@ public class GlobalExceptionHandler {
 
   @ExceptionHandler(Exception.class)
   protected ResponseEntity<ErrorResponse> handleException(Exception e) {
-    log.error("handleEntityNotFoundException", e);
+    log.error("handleException", e);
     final ErrorResponse response = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR);
     return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
   }
