675a64bcdb2e4ec4c439d1171ca42bf00135e84e
전찬의 <jcu011@naver.com>
2021-12-11 22:01:14 +0900

    refactor: EntityNotFoundException 생성자 추가

--- src/main/java/com/devnity/devnity/common/error/exception/EntityNotFoundException.java
+++ src/main/java/com/devnity/devnity/common/error/exception/EntityNotFoundException.java
@@ -2,7 +2,12 @@ package com.devnity.devnity.common.error.exception;
 
 public class EntityNotFoundException extends BusinessException {
 
+  public EntityNotFoundException(String message, ErrorCode errorCode) {
+    super(message, errorCode);
+  }
+
   public EntityNotFoundException(String message) {
     super(message, ErrorCode.ENTITY_NOT_FOUND);
   }
+
 }
