0b966c3ab60635b13f4e9dd527c2d95dd253383a
전찬의 <jcu011@naver.com>
2021-12-12 06:35:45 +0900

    feat: 맵각코 등록 컨트롤러 추가

--- src/main/java/com/devnity/devnity/domain/mapgakco/controller/MapgakcoApplicantV1Controller.java
+++ src/main/java/com/devnity/devnity/domain/mapgakco/controller/MapgakcoApplicantV1Controller.java
@@ -1,7 +1,13 @@
 package com.devnity.devnity.domain.mapgakco.controller;
 
-import com.devnity.devnity.domain.mapgakco.service.MapgakcoApplicantService;
+import com.devnity.devnity.domain.auth.jwt.JwtAuthentication;
+import com.devnity.devnity.domain.mapgakco.entity.MapgakcoStatus;
+import com.devnity.devnity.domain.mapgakco.service.mapgakcoapplicant.MapgakcoApplicantService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class MapgakcoApplicantV1Controller {
 
     private final MapgakcoApplicantService mapgakcoApplicantService;
+
+    @PostMapping("/mapgakcos/{id}/apply")
+    public ResponseEntity<MapgakcoStatus> applyMapgakco(
+      @AuthenticationPrincipal JwtAuthentication jwtAuthentication,
+      @PathVariable Long mapgakcoId
+    ) {
+        return ResponseEntity.ok(
+          mapgakcoApplicantService.applyForMapgakco(mapgakcoId, jwtAuthentication.getUserId()));
+    }
 
 }
