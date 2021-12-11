f847f269e37196af3d3a6eeb56564d1b0dd8ed72
한재원 <hanjo8813@gmail.com>
2021-12-12 00:18:49 +0900

    fix: introductionService 누락 해결

--- src/main/java/com/devnity/devnity/domain/user/controller/UserController.java
+++ src/main/java/com/devnity/devnity/domain/user/controller/UserController.java
@@ -27,6 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
   private final UserService userService;
+  private final IntroductionService introductionService;
 
   @GetMapping("/me")
   public ApiResponse<UserInfoResponse> me(@UserId Long userId) {
