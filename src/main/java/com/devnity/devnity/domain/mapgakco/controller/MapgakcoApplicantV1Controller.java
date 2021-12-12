8013a80d1b6d20faef26d25e34857f055776aa80
전찬의 <jcu011@naver.com>
2021-12-12 09:02:24 +0900

    feat: 모각코 신청 취소 컨트롤러 추가

--- src/main/java/com/devnity/devnity/domain/mapgakco/controller/MapgakcoApplicantV1Controller.java
+++ src/main/java/com/devnity/devnity/domain/mapgakco/controller/MapgakcoApplicantV1Controller.java
@@ -1,11 +1,12 @@
 package com.devnity.devnity.domain.mapgakco.controller;
 
+import com.devnity.devnity.common.api.ApiResponse;
 import com.devnity.devnity.common.config.security.jwt.JwtAuthentication;
 import com.devnity.devnity.common.config.security.resolver.UserId;
 import com.devnity.devnity.domain.mapgakco.entity.MapgakcoStatus;
 import com.devnity.devnity.domain.mapgakco.service.mapgakcoapplicant.MapgakcoApplicantService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,12 +20,20 @@ public class MapgakcoApplicantV1Controller {
     private final MapgakcoApplicantService mapgakcoApplicantService;
 
     @PostMapping("/mapgakcos/{id}/apply")
-    public ResponseEntity<MapgakcoStatus> applyMapgakco(
+    public ApiResponse<MapgakcoStatus> applyMapgakco(
       @UserId JwtAuthentication jwtAuthentication,
       @PathVariable Long mapgakcoId
     ) {
-        return ResponseEntity.ok(
+        return ApiResponse.ok(
           mapgakcoApplicantService.applyForMapgakco(mapgakcoId, jwtAuthentication.getUserId()));
+    }
+
+    @DeleteMapping("/mapgakcos/{id}/apply")
+    public ApiResponse<String> cancelMapgakco(
+      @UserId JwtAuthentication jwtAuthentication,
+      @PathVariable Long mapgakcoId) {
+        mapgakcoApplicantService.cancelForMapgakco(mapgakcoId, jwtAuthentication.getUserId());
+        return ApiResponse.ok();
     }
 
 }
