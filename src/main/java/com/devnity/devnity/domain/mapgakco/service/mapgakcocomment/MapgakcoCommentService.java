fd934db648601477ac1370a4501fadf32b9e838a
전찬의 <jcu011@naver.com>
2021-12-14 17:54:44 +0900

    feat: MapgakcoComment 추가 service코드, converter코드 작성

--- src/main/java/com/devnity/devnity/domain/mapgakco/service/mapgakcocomment/MapgakcoCommentService.java
+++ src/main/java/com/devnity/devnity/domain/mapgakco/service/mapgakcocomment/MapgakcoCommentService.java
@@ -1,6 +1,12 @@
 package com.devnity.devnity.domain.mapgakco.service.mapgakcocomment;
 
+import com.devnity.devnity.domain.mapgakco.converter.MapgakcoCommentConverter;
+import com.devnity.devnity.domain.mapgakco.dto.mapgakcocomment.request.MapgakcoCommentCreateRequest;
+import com.devnity.devnity.domain.mapgakco.entity.Mapgakco;
+import com.devnity.devnity.domain.mapgakco.entity.MapgakcoComment;
 import com.devnity.devnity.domain.mapgakco.repository.MapgakcoCommentRepository;
+import com.devnity.devnity.domain.mapgakco.service.MapgakcoServiceUtils;
+import com.devnity.devnity.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,6 +16,17 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class MapgakcoCommentService {
 
-    private final MapgakcoCommentRepository mapgakcoCommentRepository;
+  private final MapgakcoCommentRepository commentRepository;
+  private final MapgakcoCommentConverter commentConverter;
+  private final MapgakcoServiceUtils mapgakcoServiceUtils;
+
+  @Transactional
+  public void create(Long mapgakcoId, Long userId, MapgakcoCommentCreateRequest request) {
+    Mapgakco mapgakco = mapgakcoServiceUtils.findMapgakcoById(mapgakcoId);
+    User user = mapgakcoServiceUtils.findUserById(userId);
+    MapgakcoComment parent = commentRepository.findById(request.getParentId()).orElseGet(null);
+    commentRepository.save(commentConverter.toComment(mapgakco, user, parent, request));
+  }
+
 
 }
