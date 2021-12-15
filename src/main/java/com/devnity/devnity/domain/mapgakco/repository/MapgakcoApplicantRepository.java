e61aa19fd7c6c8d58bc3a561b3110def74a1a3ba
전찬의 <jcu011@naver.com>
2021-12-16 02:29:08 +0900

    feat: 쿼리메소드 다수 추가

--- src/main/java/com/devnity/devnity/domain/mapgakco/repository/MapgakcoApplicantRepository.java
+++ src/main/java/com/devnity/devnity/domain/mapgakco/repository/MapgakcoApplicantRepository.java
@@ -3,6 +3,7 @@ package com.devnity.devnity.domain.mapgakco.repository;
 import com.devnity.devnity.domain.mapgakco.entity.Mapgakco;
 import com.devnity.devnity.domain.mapgakco.entity.MapgakcoApplicant;
 import com.devnity.devnity.domain.user.entity.User;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -11,5 +12,7 @@ public interface MapgakcoApplicantRepository extends JpaRepository<MapgakcoAppli
   Optional<MapgakcoApplicant> findByMapgakcoAndUser(Mapgakco mapgakco, User user);
 
   void deleteByMapgakcoAndUser(Mapgakco mapgakco, User user);
+
+  List<MapgakcoApplicant> getByMapgakco(Mapgakco mapgakco);
 
 }
--- src/main/java/com/devnity/devnity/domain/mapgakco/repository/MapgakcoRepository.java
+++ src/main/java/com/devnity/devnity/domain/mapgakco/repository/MapgakcoRepository.java
@@ -1,8 +1,15 @@
 package com.devnity.devnity.domain.mapgakco.repository;
 
 import com.devnity.devnity.domain.mapgakco.entity.Mapgakco;
+import com.devnity.devnity.domain.mapgakco.entity.MapgakcoStatus;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MapgakcoRepository extends JpaRepository<Mapgakco, Long>, MapgakcoCustomRepository {
+
+  Optional<Mapgakco> findByIdAndStatusNot(Long id, MapgakcoStatus status);
+
+  List<Mapgakco> getByStatusNot(MapgakcoStatus status);
 
 }
