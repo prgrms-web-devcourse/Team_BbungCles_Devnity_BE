1d8b0c9c6a28603ee8a20bbffe635deafaf89ad5
전찬의 <jcu011@naver.com>
2021-12-16 06:34:27 +0900

    refactor: MapgakcoCommentConverter 수정

--- src/main/java/com/devnity/devnity/domain/mapgakco/converter/MapgakcoCommentConverter.java
+++ src/main/java/com/devnity/devnity/domain/mapgakco/converter/MapgakcoCommentConverter.java
@@ -1,9 +1,12 @@
 package com.devnity.devnity.domain.mapgakco.converter;
 
 import com.devnity.devnity.domain.mapgakco.dto.mapgakcocomment.request.MapgakcoCommentCreateRequest;
+import com.devnity.devnity.domain.mapgakco.dto.mapgakcocomment.response.MapgakcoCommentResponse;
 import com.devnity.devnity.domain.mapgakco.entity.Mapgakco;
 import com.devnity.devnity.domain.mapgakco.entity.MapgakcoComment;
+import com.devnity.devnity.domain.user.dto.SimpleUserInfoDto;
 import com.devnity.devnity.domain.user.entity.User;
+import java.util.List;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -23,5 +26,20 @@ public class MapgakcoCommentConverter {
       .build();
   }
 
+  public MapgakcoCommentResponse toMapgakcoCommentResponse(
+    MapgakcoComment comment,
+    SimpleUserInfoDto writer,
+    List<MapgakcoCommentResponse> children
+  ) {
+    return MapgakcoCommentResponse.builder()
+      .commentId(comment.getId())
+      .content(comment.getContent())
+      .status(comment.getStatus())
+      .createdAt(comment.getCreatedAt())
+      .updatedAt(comment.getModifiedAt())
+      .writer(writer)
+      .children(children)
+      .build();
+  }
 
 }
