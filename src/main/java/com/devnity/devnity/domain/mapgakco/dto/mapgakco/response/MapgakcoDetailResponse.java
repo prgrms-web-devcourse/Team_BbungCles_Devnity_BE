86c7f732e8020f6c29d6613fbb67bb3bba9cc782
전찬의 <jcu011@naver.com>
2021-12-16 06:32:29 +0900

    refactor: Response dto 수정

--- src/main/java/com/devnity/devnity/domain/mapgakco/dto/mapgakco/response/MapgakcoDetailResponse.java
+++ src/main/java/com/devnity/devnity/domain/mapgakco/dto/mapgakco/response/MapgakcoDetailResponse.java
@@ -3,21 +3,44 @@ package com.devnity.devnity.domain.mapgakco.dto.mapgakco.response;
 import com.devnity.devnity.domain.mapgakco.dto.mapgakcocomment.response.MapgakcoCommentResponse;
 import com.devnity.devnity.domain.user.dto.SimpleUserInfoDto;
 import java.util.List;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @Getter
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-@AllArgsConstructor
-@Builder
 public class MapgakcoDetailResponse {
 
   private MapgakcoResponse mapgakco;
   private SimpleUserInfoDto writer;
   private List<SimpleUserInfoDto> applicants;
   private List<MapgakcoCommentResponse> comments;
+
+  @Builder
+  public MapgakcoDetailResponse(
+    MapgakcoResponse mapgakco,
+    SimpleUserInfoDto writer,
+    List<SimpleUserInfoDto> applicants,
+    List<MapgakcoCommentResponse> comments
+  ) {
+    this.mapgakco = mapgakco;
+    this.writer = writer;
+    this.applicants = applicants;
+    this.comments = comments;
+  }
+
+  public static MapgakcoDetailResponse of(
+    MapgakcoResponse mapgakco,
+    SimpleUserInfoDto writer,
+    List<SimpleUserInfoDto> applicants,
+    List<MapgakcoCommentResponse> comments
+  ) {
+    return MapgakcoDetailResponse.builder()
+      .mapgakco(mapgakco)
+      .writer(writer)
+      .applicants(applicants)
+      .comments(comments)
+      .build();
+  }
 
 }
