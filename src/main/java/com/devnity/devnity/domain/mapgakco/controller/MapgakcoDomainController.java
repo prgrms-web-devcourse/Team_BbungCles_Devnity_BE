1153695017573058d4379951b20c406e4de7d82e
전찬의 <jcu011@naver.com>
2021-12-16 06:37:34 +0900

    feat: 컨트롤러 추가, 서비스 추가

---
+++ src/main/java/com/devnity/devnity/domain/mapgakco/controller/MapgakcoDomainController.java
@@ -0,0 +1,24 @@
+package com.devnity.devnity.domain.mapgakco.controller;
+
+import com.devnity.devnity.common.api.ApiResponse;
+import com.devnity.devnity.domain.mapgakco.dto.mapgakco.response.MapgakcoDetailResponse;
+import com.devnity.devnity.domain.mapgakco.service.MapgakcoDomainService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+@RestController
+public class MapgakcoDomainController {
+
+  private final MapgakcoDomainService domainService;
+
+  @GetMapping("/mapgakcos/{mapgakcoId}")
+  public ApiResponse<MapgakcoDetailResponse> getMapgakcoDetail(@PathVariable Long mapgakcoId) {
+    return ApiResponse.ok(domainService.getDetail(mapgakcoId));
+  }
+
+}
