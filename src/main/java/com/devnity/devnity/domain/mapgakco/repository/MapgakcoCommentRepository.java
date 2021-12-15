d0bee31ca9dad61d52879ed8adf7796559d4d5a7
전찬의 <jcu011@naver.com>
2021-12-16 02:29:08 +0900

    feat: 쿼리메소드 다수 추가

--- src/main/java/com/devnity/devnity/domain/mapgakco/repository/MapgakcoCommentRepository.java
+++ src/main/java/com/devnity/devnity/domain/mapgakco/repository/MapgakcoCommentRepository.java
@@ -1,8 +1,18 @@
 package com.devnity.devnity.domain.mapgakco.repository;
 
+import com.devnity.devnity.domain.mapgakco.entity.Mapgakco;
 import com.devnity.devnity.domain.mapgakco.entity.MapgakcoComment;
+import com.devnity.devnity.domain.mapgakco.entity.MapgakcoCommentStatus;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MapgakcoCommentRepository extends JpaRepository<MapgakcoComment, Long> {
+
+  Optional<MapgakcoComment> findByIdAndStatusNot(Long id, MapgakcoCommentStatus status);
+
+  List<MapgakcoComment> getByMapgakcoAndParentIsNull(Mapgakco mapgakco);
+
+  List<MapgakcoComment> getByParent(MapgakcoComment parent);
 
 }
