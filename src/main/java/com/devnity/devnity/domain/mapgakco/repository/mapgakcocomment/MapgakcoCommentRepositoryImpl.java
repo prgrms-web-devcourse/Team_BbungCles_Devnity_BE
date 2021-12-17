a55f25fb2ce85cc2266f39de1c6192351cc99177
전찬의 <jcu011@naver.com>
2021-12-18 00:29:56 +0900

    refactor: 생각난김에 바꾼 user의 course와 generation fetch join

--- src/main/java/com/devnity/devnity/domain/mapgakco/repository/mapgakcocomment/MapgakcoCommentRepositoryImpl.java
+++ src/main/java/com/devnity/devnity/domain/mapgakco/repository/mapgakcocomment/MapgakcoCommentRepositoryImpl.java
@@ -1,6 +1,8 @@
 package com.devnity.devnity.domain.mapgakco.repository.mapgakcocomment;
 
 import static com.devnity.devnity.domain.mapgakco.entity.QMapgakcoComment.mapgakcoComment;
+import static com.devnity.devnity.domain.user.entity.QCourse.course;
+import static com.devnity.devnity.domain.user.entity.QGeneration.generation;
 import static com.devnity.devnity.domain.user.entity.QUser.user;
 
 import com.devnity.devnity.domain.mapgakco.entity.Mapgakco;
@@ -15,11 +17,13 @@ public class MapgakcoCommentRepositoryImpl implements MapgakcoCommentRepositoryC
 
   private final JPAQueryFactory jpaQueryFactory;
 
-  @Override // Todo : user의 course, generation, Introduction fetch join
+  @Override
   public List<MapgakcoComment> getParentByMapgakcoWithUser(Mapgakco mapgakco) {
     return jpaQueryFactory
       .selectFrom(mapgakcoComment)
       .join(mapgakcoComment.user, user).fetchJoin()
+      .join(user.generation, generation).fetchJoin()
+      .join(user.course, course).fetchJoin()
       .where(mapgakcoEq(mapgakco), mapgakcoComment.parent.isNull())
       .fetch();
   }
@@ -28,11 +32,13 @@ public class MapgakcoCommentRepositoryImpl implements MapgakcoCommentRepositoryC
     return mapgakco != null ? mapgakcoComment.mapgakco.eq(mapgakco) : null;
   }
 
-  @Override // Todo : user의 course, generation, Introduction fetch join
+  @Override
   public List<MapgakcoComment> getChildByParentWithUser(MapgakcoComment parent) {
     return jpaQueryFactory
       .selectFrom(mapgakcoComment)
       .join(mapgakcoComment.user, user).fetchJoin()
+      .join(user.generation, generation).fetchJoin()
+      .join(user.course, course).fetchJoin()
       .where(parentEq(parent))
       .fetch();
   }
