663f8cd13e92da1361dafb55576d18663ab5373d
전찬의 <jcu011@naver.com>
2021-12-17 20:38:59 +0900

    feat: 맵각코 페이지 Response, Request 추가

--- src/main/java/com/devnity/devnity/domain/mapgakco/dto/mapgakco/response/MapgakcoPageResponse.java
+++ src/main/java/com/devnity/devnity/domain/mapgakco/dto/mapgakco/response/MapgakcoPageResponse.java
@@ -2,19 +2,31 @@ package com.devnity.devnity.domain.mapgakco.dto.mapgakco.response;
 
 import com.devnity.devnity.domain.mapgakco.dto.SimpleMapgakcoInfoDto;
 import java.util.List;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @Getter
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-@AllArgsConstructor
-@Builder
 public class MapgakcoPageResponse {
 
   private List<SimpleMapgakcoInfoDto> mapgakcos;
+  private Double lastDistance;
   private Boolean hasNext;
+
+  @Builder
+  public MapgakcoPageResponse(List<SimpleMapgakcoInfoDto> mapgakcos, Double lastDistance, Boolean hasNext) {
+    this.mapgakcos = mapgakcos;
+    this.lastDistance = lastDistance;
+    this.hasNext = hasNext;
+  }
+
+  public static MapgakcoPageResponse of(List<SimpleMapgakcoInfoDto> mapgakcos, Double lastDistance, Boolean hasNext) {
+    return MapgakcoPageResponse.builder()
+      .mapgakcos(mapgakcos)
+      .lastDistance(lastDistance)
+      .hasNext(hasNext)
+      .build();
+  }
 
 }
