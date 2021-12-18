f72d6178353c0f3019689e576d9b3e29dfa19f12
전찬의 <jcu011@naver.com>
2021-12-19 06:32:13 +0900

    feat: 데둥여지도 조회 비즈니스 로직 추가

--- src/main/java/com/devnity/devnity/domain/user/repository/UserCustomRepositoryImpl.java
+++ src/main/java/com/devnity/devnity/domain/user/repository/UserCustomRepositoryImpl.java
@@ -5,11 +5,11 @@ import static com.devnity.devnity.domain.user.entity.QCourse.course;
 import static com.devnity.devnity.domain.user.entity.QGeneration.generation;
 import static com.devnity.devnity.domain.user.entity.QUser.user;
 
-import com.devnity.devnity.domain.introduction.entity.QIntroduction;
-import com.devnity.devnity.domain.user.entity.QCourse;
-import com.devnity.devnity.domain.user.entity.QGeneration;
+import com.devnity.devnity.domain.user.entity.Course;
+import com.devnity.devnity.domain.user.entity.Generation;
 import com.devnity.devnity.domain.user.entity.User;
 import com.devnity.devnity.domain.user.entity.UserStatus;
+import com.querydsl.core.types.Predicate;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -26,14 +26,38 @@ public class UserCustomRepositoryImpl implements UserCustomRepository {
         .join(user.generation, generation).fetchJoin()
         .join(user.course, course).fetchJoin()
         .join(user.introduction, introduction).fetchJoin()
-        .where(
-          user.course.eq(inputUser.getCourse()),
-          user.generation.eq(inputUser.getGeneration()),
-          user.ne(inputUser),
-          user.status.eq(UserStatus.ACTIVE)
-        )
-        .orderBy(user.id.desc())
-        .limit(size)
-        .fetch();
+      .where(
+        user.course.eq(inputUser.getCourse()),
+        user.generation.eq(inputUser.getGeneration()),
+        user.ne(inputUser),
+        user.status.eq(UserStatus.ACTIVE)
+      )
+      .orderBy(user.id.desc())
+      .limit(size)
+      .fetch();
   }
+
+  @Override
+  public List<User> getAllByCourseAndGeneration(Course courseIn, Generation generationIn) {
+    return jpaQueryFactory
+      .selectFrom(user)
+      .join(user.generation, generation).fetchJoin()
+      .join(user.course, course).fetchJoin()
+      .join(user.introduction, introduction).fetchJoin()
+      .where(
+        courseEq(courseIn),
+        generationEq(generationIn),
+        user.status.eq(UserStatus.ACTIVE)
+      )
+      .fetch();
+  }
+
+  private Predicate courseEq(Course courseIn) {
+    return courseIn != null ? user.course.eq(courseIn) : null;
+  }
+
+  private Predicate generationEq(Generation generationIn) {
+    return generationIn != null ? user.generation.eq(generationIn) : null;
+  }
+
 }
