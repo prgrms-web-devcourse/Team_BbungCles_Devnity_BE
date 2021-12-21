2451e27d28ba05b3cc4fef73527b231ca7178e3c
전찬의 <jcu011@naver.com>
2021-12-21 15:20:49 +0900

    test: 맵각코 생성 테스트코드 수정

--- src/main/java/com/devnity/devnity/domain/mapgakco/dto/mapgakco/response/MapgakcoCreateResponse.java
+++ src/main/java/com/devnity/devnity/domain/mapgakco/dto/mapgakco/response/MapgakcoCreateResponse.java
@@ -2,15 +2,12 @@ package com.devnity.devnity.domain.mapgakco.dto.mapgakco.response;
 
 import com.devnity.devnity.domain.mapgakco.entity.MapgakcoStatus;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-@AllArgsConstructor
-@Builder
 public class MapgakcoCreateResponse {
 
   private Long mapgakcoId;
