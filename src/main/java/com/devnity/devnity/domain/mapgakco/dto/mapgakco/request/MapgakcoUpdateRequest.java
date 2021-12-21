6a99a33a0f7400bbf11cc34b3aa673084251f067
전찬의 <jcu011@naver.com>
2021-12-21 18:29:35 +0900

    refactor: validator 몇개 추가

--- src/main/java/com/devnity/devnity/domain/mapgakco/dto/mapgakco/request/MapgakcoUpdateRequest.java
+++ src/main/java/com/devnity/devnity/domain/mapgakco/dto/mapgakco/request/MapgakcoUpdateRequest.java
@@ -1,7 +1,10 @@
 package com.devnity.devnity.domain.mapgakco.dto.mapgakco.request;
 
+import com.devnity.devnity.common.utils.annotation.Title;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,10 +17,15 @@ import lombok.NoArgsConstructor;
 @Builder
 public class MapgakcoUpdateRequest {
 
+  @Title
   private String title;
+  @NotBlank
   private String content;
+  @NotNull
   private String location;
+  @NotNull
   private Double latitude;
+  @NotNull
   private Double longitude;
   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
   private LocalDateTime meetingAt;
